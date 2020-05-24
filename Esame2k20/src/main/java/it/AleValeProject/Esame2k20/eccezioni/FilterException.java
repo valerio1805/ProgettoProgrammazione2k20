704501316d407a3f6cedb876987899e69d9ff59b
alessandrodibiase <s1087053@gmail.com>
2020-05-24 12:46:57 +0200

    c'era una parentesi di troppo

--- Esame2k20/src/main/java/it/AleValeProject/Esame2k20/eccezioni/FilterException.java
+++ Esame2k20/src/main/java/it/AleValeProject/Esame2k20/eccezioni/FilterException.java
@@ -1,8 +1,7 @@
 package it.AleValeProject.Esame2k20.eccezioni;
 
-public class FilterException extends Exception{
-		public FilterException(String errore){
-			super("CORREGERE FILTRO: "+errore);
-		}
+public class FilterException extends Exception {
+	public FilterException(String errore) {
+		super("CORREGERE FILTRO: " + errore);
 	}
 }
