d54db24c2f9ef36d271ad548b652dfd3a85cbc4d
alessandrodibiase <s1087053@gmail.com>
2020-05-26 11:43:22 +0200

    controller per filtri

--- Esame2k20/src/main/java/it/AleValeProject/Esame2k20/controller/Controller.java
+++ Esame2k20/src/main/java/it/AleValeProject/Esame2k20/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,11 @@ public class Controller {
 	}
 	
 	
-	//@GetMapping("/GetDataWithFilter/")
+	@GetMapping("/GetData/{filter}&{field}")
+	public ResponseEntity<Object> getDataWithFilters(@PathVariable ("filter")String filtro, @PathVariable ("field")String campo){
+		return new ResponseEntity<>(serviziodati.VisalizzaData(),HttpStatus.OK);
+		
+	}
 	
 	
 	//@GetMapping("/GetFieldStats")
